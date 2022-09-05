eee4ebb1ce698e1a022165e02e1da1cd7b4a57a8
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-05 20:42:54 +0300

    add Homework1

---
+++ Homework1/Task1.py
@@ -0,0 +1,8 @@
+#Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+n=int(input('Input number from 1 to 7 ' ))
+if n==6 or n==7:
+    print('Yes')
+elif 6>n>0:
+    print('No')
+else:
+    print('There is no such day of the week!')
