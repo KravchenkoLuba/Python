ce46464d55ac4963310a695bab0b0d055310038e
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-05 23:43:34 +0300

    add Task2

--- Homework1/Task2.py
+++ Homework1/Task2.py
@@ -1,1 +1,13 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+x = int(input('Введите 1 или 0 '))
+y = int(input('Введите 1 или 0 '))
+z = int(input('Введите 1 или 0 '))
+if (not (x or y or z)) == (not x and not y and not z):
+    print(True)
+else:
+    print(False)    
+
+
+
+
+
