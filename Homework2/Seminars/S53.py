11b52f499c8ed4e07802a898a15d2d7a1997b6f3
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-19 19:54:02 +0300

    corrected cod

--- Homework2/Seminars/S53.py
+++ Homework2/Seminars/S53.py
@@ -1,8 +1,2 @@
-with open("main.txt","r") as file_obj:
-    for line in file_obj:
-        if '5' in line:
-            line = line.upper()
-        list_student.append(line.replace('\n',''))
-with open("main.txt", "w") as file_obj:
-    for line in list_student:
-        file_obj.write(line + '\n')
+
+
