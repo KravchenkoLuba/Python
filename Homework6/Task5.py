7f6e736dbc8d8f0a238dd5db44569c109e501ffc
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-25 00:07:16 +0300

    solved all tasks!

--- Homework6/Task5.py
+++ Homework6/Task5.py
@@ -1,1 +1,9 @@
 #  Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+import math
+list_num = [11, 2, 7, 4, 5, 6]
+res = [(list_num[i] * list_num[-i-1]) for i in range(math.ceil(len(list_num)/2))]
+print(res)
+
+
+
+
