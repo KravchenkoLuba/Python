6a5a64fc845e8584c708dc03d892c29e4350f9f3
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-24 22:54:36 +0300

    solved task

--- Homework6/Task1.py
+++ Homework6/Task1.py
@@ -2,7 +2,7 @@
 # ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
 
 lst = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
-a = '6'
+a = '24'
 res = list(filter(lambda x: a in x, lst))
 if len(res) > 0:
     print(f'{a} in the {res} element(s)')
